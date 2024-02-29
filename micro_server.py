656ff662f0f98528e64bc2b28f44cd43de388d47
sullandr <sullandr@oregonstate.edu>
2024-02-28 19:57:01 -0800

    updated

--- micro_server.py
+++ micro_server.py
@@ -6,12 +6,10 @@ import time
 
 from flask import Flask, json
 
-#companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
-
 api = Flask(__name__)
 
 @api.route('/cities', methods=['GET'])
-def get_companies():
+def get_cities():
     with open("./CS-361/cities.json",'r') as file:
         cities = json.load(file)
         keys = list(cities.keys())
@@ -30,22 +28,3 @@ def get_companies():
 
 if __name__ == '__main__':
     api.run(debug=True, port=5001)
-#if __name__ == '__main__':
-    #run_server = 'python -m http.server'
-    #os.system(run_server)
-    #while True:
-    #    time.sleep(1)
-    #    with open("./CS-361/cities.json",'r') as file:
-    #        cities = json.load(file)
-    #        keys = list(cities.keys())
-    #        rand_key = int(random.random() * 1000 % 51)        
-    #        state = (keys[rand_key])
-    #        print(state)
-    #        city = (cities[state])
-    #        rand_city = int(random.random() * 1000 % len(city))
-    #        location = city[rand_city]
-    #        print(location)
-    #        response = {"city":location, "state":state}
-    #        print(response)
-    #        response_json = json.dumps(response)
-    #        print(response_json)
